445a06372307fd52eb87fc3edeb74e4bd068621e
silent10z <dkgueing@gmail.com>
2022-07-14 23:01:43 +0900

    변수

---
+++ variable/index.js
@@ -0,0 +1,22 @@
+// 변수: 하나의 값을 저장하기 위해 확보한 메모리 공간자체 혹은 식별하기 위해 붙인 이름
+
+// 변수는 하나의 값을 저장하기 위한 수단이다
+// 변수명    변수값
+var userId = 1;
+// 초기화
+console.log(result);
+var result;
+//할당
+result = 10 + 30;
+console.log(result);
+
+// 식별자: 메모리 주소에 붙인 이름 메모리 상에 존재하는 어떤 값을 식별할 수 있는 이름은 모두 식별자라고 한다.
+
+var score;
+// 이름을 등록하고 값을 저장할 메모리 공간을 확보함 (할당은 하지 않음)
+// 암묵적 할당에 의해서 undefined값이 메모리에 할당되서 초기화 됨
+console.log(score);
+
+console.log(host);
+
+var host;
